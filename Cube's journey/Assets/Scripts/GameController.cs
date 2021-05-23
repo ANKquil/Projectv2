03d9f89eab085edfbdeb5af18f6f5208724dc64c
ANKquil <thekolinfell@gmail.com>
2021-05-23 13:23:05 +0300

    create 3 main scripts GameController, PlayerController, CamController

---
+++ Cube's journey/Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Main
+    public GameObject mainCamera;
+    public GameObject player;
+    public GameObject level;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
